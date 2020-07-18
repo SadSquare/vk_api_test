b109cd3af28f00244e5ab895b6b23c8e21cc93cd
SadSquare <ya.iliagusev1@gmail.com>
2020-07-18 12:29:41 +0300

    Bot

---
+++ test.py
@@ -0,0 +1,57 @@
+'''import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+import random 
+from random import randint
+
+def write_msg(user_id, message):
+    vk.method('message.send',{'user_id':user_id,'message':message})
+    token = "75a28e576079c89b55971015cf685314d4f93f42a8302ab3e774a4307f6bd2bb103b768f2423e8b9b1000"
+    
+    vk = vk_api.VkApi(token = token)
+    longpoll = VkLongPoll(vk)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            if event.to_me:
+                request = event.text
+
+                if request == "roll" or "Roll" or "ROLL" or "/roll":
+                    roll = random.randint(0, 100)
+                    roll = str(roll)
+                    roll = "Roll: " + roll
+                    write_msg(event.user_id, roll)
+                else:
+                    write_msg(event.user_id, "...")
+write_msg()
+'''
+import requests
+import vk_api
+from vk_api import VkUpload
+from vk_api.longpoll import VkLongPoll, VkEventType
+import random 
+from random import randint
+token = "75a28e576079c89b55971015cf685314d4f93f42a8302ab3e774a4307f6bd2bb103b768f2423e8b9b1000"
+vk_session = vk_api.VkApi(token=token)
+longpoll = VkLongPoll(vk_session)
+vk = vk_session.get_api()
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+   #Слушаем longpoll, если пришло сообщение то:			
+        if event.text == 'roll' or event.text == '/roll': #Если написали заданную фразу
+            if event.from_user:
+                roll = random.randint(0, 100)
+                roll = str(roll)
+                roll = "Roll: " + roll
+                
+
+                vk.messages.send(user_id=event.user_id, message=roll, random_id = 0)
+            elif event.from_chat:
+                vk.messages.send(chat_od = event.chat_id, message = "...")
+        else:
+            if event.from_user:
+                roll = "Дебил я ролю вообще-то "
+                vk.messages.send(user_id=event.user_id ,message=roll, random_id = 0)
+            
+        
+
+    
