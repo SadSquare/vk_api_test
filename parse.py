b48d7b0ddaf760bb19fe5e9c4f7af029282dea24
SadSquare <ya.iliagusev1@gmail.com>
2020-07-19 12:01:26 +0300

    End

--- parse.py
+++ parse.py
@@ -8,15 +8,12 @@ import time
 
 def parser(id_obj):
     url = 'https://a55.agorov.org/'
-    page = requests.get(url)
+    headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'}
+    page = requests.get(url, headers = headers)
     soup = BeautifulSoup(page.content, 'html.parser')
 
     result = soup.find(id = id_obj).text
     return result
-
-def ran_func_int(rand_range_x, rand_range_y):
-    random_num = str(random.randint(rand_range_x, rand_range_y))
-    return random_num
 
 def message_vk(mes):
     token = "20df76bd47c10e08c770927485ec32df51c7d8833a9df961151e59e910abfce2dc3a277c063f19e8f2b1a"
@@ -50,35 +47,3 @@ def main():
                 return main()
 if __name__ == "__main__":
     main()
-
-'''     
-while True:
-    if datetime.today().strftime("%A") == 'Sunday':
-        rasp = parser('raspisSun')
-        message_vk(rasp)
-    if datetime.today().strftime("%A") == 'Monday':
-        rasp = parser('raspisMon')
-        message_vk(rasp)
-        
-    if datetime.today().strftime("%A") == 'Tuesday':
-        rasp = parser('raspisTue')
-        message_vk(rasp)
-        
-    if datetime.today().strftime("%A") == 'Wednesday':
-        rasp = parser('raspisWed')
-        message_vk(rasp)
-        
-    if datetime.today().strftime("%A") == 'Thursday':
-        rasp = parser('raspisThu')
-        message_vk(rasp)
-        
-    if datetime.today().strftime("%A") == 'Friday':
-        rasp = parser('raspisFri')
-        message_vk(rasp)
-    if datetime.today().strftime("%A") == 'Saturday':
-        rasp = parser('raspisSat')
-        message_vk(rasp)
-    else:
-        message_vk("Бот поломался") 
-        break
-    time.sleep(86400)'''
